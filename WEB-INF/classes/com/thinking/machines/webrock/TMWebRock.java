e1d76c6bee6ba9875568f66abf67981750c73adb
pshrote-afk <pshrote1@gmail.com>
2025-11-28 17:09:32 +0530

    #12 Format TMWebRock, TMWebRockStarter to Google Java Style

--- WEB-INF/classes/com/thinking/machines/webrock/TMWebRock.java
+++ WEB-INF/classes/com/thinking/machines/webrock/TMWebRock.java
@@ -10,223 +10,208 @@ import com.thinking.machines.webrock.pojo.*;
 import com.thinking.machines.webrock.model.*;
 import com.thinking.machines.webrock.annotations.*;
 
-public class TMWebRock extends HttpServlet
-{
+public class TMWebRock extends HttpServlet {
 
-public void service(HttpServletRequest request, HttpServletResponse response)
-{
-    try {
-        String uri = request.getRequestURI(); 	//eg. "/tmwebrock/schoolservice/student/update"
-        String parts[] = uri.split("schoolservice");
-        String clientPath = parts[1];				//eg. "/student/update"
-        System.out.println("uri: "+request.getRequestURI());
-        System.out.println("Client path: "+clientPath);
+    public void service(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String uri = request.getRequestURI(); // eg. "/tmwebrock/schoolservice/student/update"
+            String parts[] = uri.split("schoolservice");
+            String clientPath = parts[1]; // eg. "/student/update"
+            System.out.println("uri: " + request.getRequestURI());
+            System.out.println("Client path: " + clientPath);
 
-        ServletContext servletContext = getServletContext();
-        Map<String,Service> servicesMap = (HashMap)servletContext.getAttribute("ServicesMap");
+            ServletContext servletContext = getServletContext();
+            Map<String, Service> servicesMap = (HashMap) servletContext.getAttribute("ServicesMap");
 
-        Service serviceObject = servicesMap.get(clientPath);
-        Class<?> serviceClass = serviceObject.getServiceClass();
-        Method service = serviceObject.getService();
+            Service serviceObject = servicesMap.get(clientPath);
+            Class<?> serviceClass = serviceObject.getServiceClass();
+            Method service = serviceObject.getService();
 
-        String method = request.getMethod().toUpperCase();
-        String serviceLevelMethod = "";
-        String serviceClassLevelMethod = "";
+            String method = request.getMethod().toUpperCase();
+            String serviceLevelMethod = "";
+            String serviceClassLevelMethod = "";
 
-        // method-level get/post will get precedence over class-level get/post. 
-        // eg. class has get annotation, but its method has post annotation.
+            // method-level get/post will get precedence over class-level get/post.
+            // eg. class has get annotation, but its method has post annotation.
 
-        //Check 1/3
-        if(service.isAnnotationPresent(Get.class) || service.isAnnotationPresent(Post.class))
-        {
-            if(service.isAnnotationPresent(Get.class) )
-            {
-                serviceLevelMethod = "Get";
+            // Check 1/3
+            if (service.isAnnotationPresent(Get.class) || service.isAnnotationPresent(Post.class)) {
+                if (service.isAnnotationPresent(Get.class)) {
+                    serviceLevelMethod = "Get";
+                }
+                if (service.isAnnotationPresent(Post.class)) {
+                    serviceLevelMethod = "Post";
+                }
+                if (method.equalsIgnoreCase(serviceLevelMethod)) {
+                    switch (serviceLevelMethod) {
+                        case "Get":
+                            doGet(request, response);
+                            break;
+                        case "Post":
+                            doPost(request, response);
+                            break;
+                    }
+                    return;
+                } else {
+                    response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+                    return;
+                }
+
             }
-            if(service.isAnnotationPresent(Post.class) )
-            {
-                serviceLevelMethod = "Post";
+            // Check 2/3
+            else if (serviceClass.isAnnotationPresent(Get.class) || serviceClass.isAnnotationPresent(Post.class)) {
+                if (serviceClass.isAnnotationPresent(Get.class)) {
+                    serviceClassLevelMethod = "Get";
+                }
+                if (serviceClass.isAnnotationPresent(Post.class)) {
+                    serviceClassLevelMethod = "Post";
+                }
+                if (method.equalsIgnoreCase(serviceClassLevelMethod)) {
+                    switch (serviceClassLevelMethod) {
+                        case "Get":
+                            doGet(request, response);
+                            break;
+                        case "Post":
+                            doPost(request, response);
+                            break;
+                    }
+                    return;
+                } else {
+                    response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+                    return;
+                }
             }
-            if(method.equalsIgnoreCase(serviceLevelMethod))
-            {
-                switch(serviceLevelMethod)
-                {
-                    case "Get": doGet(request,response); break;
-                    case "Post": doPost(request,response); break;
-                }   
-                return;
+            // Check 3/3
+            else {
+                switch (method) {
+                    case "GET":
+                        doGet(request, response);
+                        break;
+                    case "POST":
+                        doPost(request, response);
+                        break;
+                    default:
+                        response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+                        return; // eg. if client sends invalid method = "xyz"
+                }
             }
-            else
-            {
-                response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-		return;
-            }
-            
+        } catch (Exception exception) {
+            System.out.println("TMWebRock.java service(): " + exception);
         }
-        //Check 2/3
-        else if(serviceClass.isAnnotationPresent(Get.class) || serviceClass.isAnnotationPresent(Post.class))
-        {
-            if(serviceClass.isAnnotationPresent(Get.class) )
-            {
-                serviceClassLevelMethod = "Get";
-            }
-            if(serviceClass.isAnnotationPresent(Post.class) )
-            {
-                serviceClassLevelMethod = "Post";
-            }
-            if(method.equalsIgnoreCase(serviceClassLevelMethod))
-            {
-                switch(serviceClassLevelMethod)
-                {
-                    case "Get": doGet(request,response); break;
-                    case "Post": doPost(request,response); break;
-                }   
-                return;
-            }
-            else
-            {
-                response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-		return;
-            }
-        }
-        //Check 3/3
-        else
-        {
-            switch(method)
-            {
-                case "GET": doGet(request,response); break;
-                case "POST": doPost(request,response); break;
-                default: response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED); return; //eg. if client sends invalid method = "xyz"
-            }   
-        }        
-    } catch (Exception exception) {
-        System.out.println("TMWebRock.java service(): "+exception);
+
     }
 
-}
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("Routed to/ran doGet");
+        try {
+            PrintWriter pw = response.getWriter();
+            response.setContentType("text/plain");
 
-public void doGet(HttpServletRequest request, HttpServletResponse response)
-{
-System.out.println("Routed to/ran doGet");
-try
-{
-PrintWriter pw = response.getWriter();
-response.setContentType("text/plain");
+            // part 1
+            String uri = request.getRequestURI(); // eg. "/tmwebrock/schoolservice/student/update"
+            String parts[] = uri.split("schoolservice");
+            String clientPath = parts[1]; // eg. "/student/update"
+            System.out.println("uri: " + request.getRequestURI());
+            System.out.println("Client path: " + clientPath);
 
-//part 1
-String uri = request.getRequestURI(); 	//eg. "/tmwebrock/schoolservice/student/update"
-String parts[] = uri.split("schoolservice");
-String clientPath = parts[1];				//eg. "/student/update"
-System.out.println("uri: "+request.getRequestURI());
-System.out.println("Client path: "+clientPath);
+            // part 2
+            ServletContext servletContext = getServletContext();
+            Map<String, Service> servicesMap = (HashMap) servletContext.getAttribute("ServicesMap");
 
-//part 2
-ServletContext servletContext = getServletContext();
-Map<String,Service> servicesMap = (HashMap)servletContext.getAttribute("ServicesMap");
+            Service serviceObject = servicesMap.get(clientPath);
+            Class<?> serviceClass = serviceObject.getServiceClass();
+            Method service = serviceObject.getService();
+            String forwardTo = serviceObject.getForwardTo();
 
-Service serviceObject = servicesMap.get(clientPath);
-Class<?> serviceClass = serviceObject.getServiceClass();
-Method service = serviceObject.getService();
-String forwardTo = serviceObject.getForwardTo();
+            System.out.println("Forward to: " + forwardTo);
 
-System.out.println("Forward to: "+forwardTo);
+            // part 3
+            Object obj1 = serviceClass.getDeclaredConstructor().newInstance();
+            Object returnResponse;
+            returnResponse = service.invoke(obj1);
+            if (returnResponse == null)
+                returnResponse = ""; // return empty string if "service" has "void" return type
 
+            if (forwardTo != null) {
+                Service serviceForwardObject = servicesMap.get(forwardTo);
+                if (serviceForwardObject != null) {
+                    System.out.println("Entered manual obj2 invocation");
+                    serviceClass = serviceForwardObject.getServiceClass();
+                    service = serviceForwardObject.getService();
 
-//part 3
-Object obj1 = serviceClass.getDeclaredConstructor().newInstance();
-Object returnResponse;
-returnResponse = service.invoke(obj1);
-if(returnResponse==null) returnResponse = "";	//return empty string if "service" has "void" return type
+                    Object obj2 = serviceClass.getDeclaredConstructor().newInstance();
+                    returnResponse = service.invoke(obj2);
+                    if (returnResponse == null)
+                        returnResponse = ""; // return empty string if "service" has "void" return type
+                } else {
+                    System.out.println("Entered forward to a regular servlet");
+                    RequestDispatcher requestDispatcher;
+                    requestDispatcher = request.getRequestDispatcher(forwardTo);
+                    requestDispatcher.forward(request, response);
+                }
+            }
 
-if(forwardTo!=null)
-{
-Service serviceForwardObject = servicesMap.get(forwardTo);
-if(serviceForwardObject!=null)
-{
-System.out.println("Entered manual obj2 invocation");
-serviceClass = serviceForwardObject.getServiceClass();
-service = serviceForwardObject.getService();
+            pw.print(returnResponse);
+            pw.flush();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 
-Object obj2 = serviceClass.getDeclaredConstructor().newInstance();
-returnResponse = service.invoke(obj2);
-if(returnResponse==null) returnResponse = "";	//return empty string if "service" has "void" return type
-}
-else
-{
-System.out.println("Entered forward to a regular servlet");
-RequestDispatcher requestDispatcher;
-requestDispatcher = request.getRequestDispatcher(forwardTo);
-requestDispatcher.forward(request,response);
-}
-}
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("Routed to/ran doPost");
+        try {
+            PrintWriter pw = response.getWriter();
+            response.setContentType("text/plain");
 
-pw.print(returnResponse);
-pw.flush();
-}catch(Exception e)
-{
-System.out.println(e);
-}
-}
-public void doPost(HttpServletRequest request, HttpServletResponse response)
-{
-System.out.println("Routed to/ran doPost");
-try
-{
-PrintWriter pw = response.getWriter();
-response.setContentType("text/plain");
+            // part 1
+            String uri = request.getRequestURI(); // eg. "/tmwebrock/schoolservice/student/update"
+            String parts[] = uri.split("schoolservice");
+            String clientPath = parts[1]; // eg. "/student/update"
+            System.out.println("uri: " + request.getRequestURI());
+            System.out.println("Client path: " + clientPath);
 
-//part 1
-String uri = request.getRequestURI(); 	//eg. "/tmwebrock/schoolservice/student/update"
-String parts[] = uri.split("schoolservice");
-String clientPath = parts[1];				//eg. "/student/update"
-System.out.println("uri: "+request.getRequestURI());
-System.out.println("Client path: "+clientPath);
+            // part 2
+            ServletContext servletContext = getServletContext();
+            Map<String, Service> servicesMap = (HashMap) servletContext.getAttribute("ServicesMap");
 
-//part 2
-ServletContext servletContext = getServletContext();
-Map<String,Service> servicesMap = (HashMap)servletContext.getAttribute("ServicesMap");
+            Service serviceObject = servicesMap.get(clientPath);
+            Class<?> serviceClass = serviceObject.getServiceClass();
+            Method service = serviceObject.getService();
+            String forwardTo = serviceObject.getForwardTo();
 
-Service serviceObject = servicesMap.get(clientPath);
-Class<?> serviceClass = serviceObject.getServiceClass();
-Method service = serviceObject.getService();
-String forwardTo = serviceObject.getForwardTo();
+            System.out.println("Forward to: " + forwardTo);
 
-System.out.println("Forward to: "+forwardTo);
+            // part 3
+            Object obj1 = serviceClass.getDeclaredConstructor().newInstance();
+            Object returnResponse;
+            returnResponse = service.invoke(obj1);
+            if (returnResponse == null)
+                returnResponse = ""; // return empty string if "service" has "void" return type
 
+            if (forwardTo != null) {
+                Service serviceForwardObject = servicesMap.get(forwardTo);
+                if (serviceForwardObject != null) {
+                    System.out.println("Entered manual obj2 invocation");
+                    serviceClass = serviceForwardObject.getServiceClass();
+                    service = serviceForwardObject.getService();
 
-//part 3
-Object obj1 = serviceClass.getDeclaredConstructor().newInstance();
-Object returnResponse;
-returnResponse = service.invoke(obj1);
-if(returnResponse==null) returnResponse = "";	//return empty string if "service" has "void" return type
+                    Object obj2 = serviceClass.getDeclaredConstructor().newInstance();
+                    returnResponse = service.invoke(obj2);
+                    if (returnResponse == null)
+                        returnResponse = ""; // return empty string if "service" has "void" return type
+                } else {
+                    System.out.println("Entered forward to a regular servlet");
+                    RequestDispatcher requestDispatcher;
+                    requestDispatcher = request.getRequestDispatcher(forwardTo);
+                    requestDispatcher.forward(request, response);
+                }
+            }
 
-if(forwardTo!=null)
-{
-Service serviceForwardObject = servicesMap.get(forwardTo);
-if(serviceForwardObject!=null)
-{
-System.out.println("Entered manual obj2 invocation");
-serviceClass = serviceForwardObject.getServiceClass();
-service = serviceForwardObject.getService();
-
-Object obj2 = serviceClass.getDeclaredConstructor().newInstance();
-returnResponse = service.invoke(obj2);
-if(returnResponse==null) returnResponse = "";	//return empty string if "service" has "void" return type
-}
-else
-{
-System.out.println("Entered forward to a regular servlet");
-RequestDispatcher requestDispatcher;
-requestDispatcher = request.getRequestDispatcher(forwardTo);
-requestDispatcher.forward(request,response);
-}
-}
-
-pw.print(returnResponse);
-pw.flush();
-}catch(Exception e)
-{
-System.out.println(e);
-}
-} //end of post
-} //end of class
+            pw.print(returnResponse);
+            pw.flush();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    } // end of post
+} // end of class
--- WEB-INF/classes/com/thinking/machines/webrock/TMWebRockStarter.java
+++ WEB-INF/classes/com/thinking/machines/webrock/TMWebRockStarter.java
@@ -10,101 +10,95 @@ import com.thinking.machines.webrock.pojo.*;
 import com.thinking.machines.webrock.model.*;
 import com.thinking.machines.webrock.annotations.*;
 
-public class TMWebRockStarter extends HttpServlet
-{
+public class TMWebRockStarter extends HttpServlet {
 
-private void scanRecursive(String pkg, Set<Class<?>> classes) throws URISyntaxException, ClassNotFoundException
-{
-String path = pkg.replace(".","/");	//eg. convert bobby.test to bobby/test
-URL url = Thread.currentThread().getContextClassLoader().getResource(path); 	//Ask Tomcat’s classloader: “Give me the folder on classpath that matches this path.” eg. bobby/util to /WEB-INF/classes/bobby/util
-File dir = new File(url.toURI());		//convert URL to File object
+    private void scanRecursive(String pkg, Set<Class<?>> classes) throws URISyntaxException, ClassNotFoundException {
+        String path = pkg.replace(".", "/"); // eg. convert bobby.test to bobby/test
+        URL url = Thread.currentThread().getContextClassLoader().getResource(path); // Ask Tomcat’s classloader: “Give
+                                                                                    // me the folder on classpath that
+                                                                                    // matches this path.” eg.
+                                                                                    // bobby/util to
+                                                                                    // /WEB-INF/classes/bobby/util
+        File dir = new File(url.toURI()); // convert URL to File object
 
-for(File f: dir.listFiles())
-{
-if(f.isDirectory())
-{
-scanRecursive(pkg + "." + f.getName(), classes);
-continue;
-}
-if(f.getName().endsWith(".class"))
-{
-String className = f.getName().replace(".class","");
-System.out.println("Class name: "+className);
-System.out.println("Class name w/ package: "+ pkg+"."+className);
+        for (File f : dir.listFiles()) {
+            if (f.isDirectory()) {
+                scanRecursive(pkg + "." + f.getName(), classes);
+                continue;
+            }
+            if (f.getName().endsWith(".class")) {
+                String className = f.getName().replace(".class", "");
+                System.out.println("Class name: " + className);
+                System.out.println("Class name w/ package: " + pkg + "." + className);
 
-Class<?> c1 = Class.forName(pkg + "." + className);
-if(c1.isAnnotationPresent(Path.class))		//masterstroke: if class doesnt have Path annotation, it's methods wont be scanned
-{
-classes.add(c1);
-}
-}
-}
-}
+                Class<?> c1 = Class.forName(pkg + "." + className);
+                if (c1.isAnnotationPresent(Path.class)) // masterstroke: if class doesnt have Path annotation, it's
+                                                        // methods wont be scanned
+                {
+                    classes.add(c1);
+                }
+            }
+        }
+    }
 
-public void init() 
-{
-System.out.println("TMWebRockStarter\nTMWebRockStarter\nTMWebRockStarter\nTMWebRockStarter\nTMWebRockStarter\nTMWebRockStarter");
-try
-{
-WebRockModel webRockModel = new WebRockModel();
-Map<String,Service> servicesMap = webRockModel.getServicesMap();
+    public void init() {
+        System.out.println(
+                "TMWebRockStarter\nTMWebRockStarter\nTMWebRockStarter\nTMWebRockStarter\nTMWebRockStarter\nTMWebRockStarter");
+        try {
+            WebRockModel webRockModel = new WebRockModel();
+            Map<String, Service> servicesMap = webRockModel.getServicesMap();
 
-ServletContext servletContext = getServletContext();
-String servicePackagePrefix = servletContext.getInitParameter("SERVICE_PACKAGE_PREFIX"); 
+            ServletContext servletContext = getServletContext();
+            String servicePackagePrefix = servletContext.getInitParameter("SERVICE_PACKAGE_PREFIX");
 
-System.out.println(servicePackagePrefix);
+            System.out.println(servicePackagePrefix);
 
-Set<Class<?>> classes = new HashSet<>();
-this.scanRecursive(servicePackagePrefix, classes);
-// all classes under "bobby", which has "Path" annotation, loaded into "classes"
+            Set<Class<?>> classes = new HashSet<>();
+            this.scanRecursive(servicePackagePrefix, classes);
+            // all classes under "bobby", which has "Path" annotation, loaded into "classes"
 
-// now iterate through each class' methods
-Service service;
-String classPathAnnotation;
-String methodPathAnnotation;
-for(Class<?> c1: classes)
-{
-for(Method m1: c1.getDeclaredMethods())
-{
-if(m1.isAnnotationPresent(Path.class))
-{
-classPathAnnotation = c1.getAnnotation(Path.class).value();
-methodPathAnnotation = m1.getAnnotation(Path.class).value();
+            // now iterate through each class' methods
+            Service service;
+            String classPathAnnotation;
+            String methodPathAnnotation;
+            for (Class<?> c1 : classes) {
+                for (Method m1 : c1.getDeclaredMethods()) {
+                    if (m1.isAnnotationPresent(Path.class)) {
+                        classPathAnnotation = c1.getAnnotation(Path.class).value();
+                        methodPathAnnotation = m1.getAnnotation(Path.class).value();
 
-service = new Service();
-service.setServiceClass(c1);
-String fullPath = classPathAnnotation + methodPathAnnotation;
-service.setPath(fullPath);
-service.setService(m1);
-if(m1.isAnnotationPresent(Forward.class)) service.setForwardTo(m1.getAnnotation(Forward.class).value());
+                        service = new Service();
+                        service.setServiceClass(c1);
+                        String fullPath = classPathAnnotation + methodPathAnnotation;
+                        service.setPath(fullPath);
+                        service.setService(m1);
+                        if (m1.isAnnotationPresent(Forward.class))
+                            service.setForwardTo(m1.getAnnotation(Forward.class).value());
 
-servicesMap.put(fullPath, service);
-}
-}
-}
-servletContext.setAttribute("ServicesMap",servicesMap);
+                        servicesMap.put(fullPath, service);
+                    }
+                }
+            }
+            servletContext.setAttribute("ServicesMap", servicesMap);
 
-//testing
-Service service1;
-Iterator<Map.Entry<String,Service>> iterator1 = servicesMap.entrySet().iterator();
-System.out.println("");
-while(iterator1.hasNext())
-{
-Map.Entry<String,Service> entry1 = iterator1.next();
-System.out.println("Key: " + entry1.getKey());
-service = entry1.getValue();
-System.out.println(service.getServiceClass());
-System.out.println(service.getPath());
-System.out.println(service.getService());
-System.out.println("Forward to: "+service.getForwardTo());
-System.out.println("");
+            // testing
+            Service service1;
+            Iterator<Map.Entry<String, Service>> iterator1 = servicesMap.entrySet().iterator();
+            System.out.println("");
+            while (iterator1.hasNext()) {
+                Map.Entry<String, Service> entry1 = iterator1.next();
+                System.out.println("Key: " + entry1.getKey());
+                service = entry1.getValue();
+                System.out.println(service.getServiceClass());
+                System.out.println(service.getPath());
+                System.out.println(service.getService());
+                System.out.println("Forward to: " + service.getForwardTo());
+                System.out.println("");
 
-}
+            }
 
-
-}catch(Exception exception)
-{
-System.out.println(exception.getMessage());
-}
-} //end of init()
-} //end of class
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    } // end of init()
+} // end of class
